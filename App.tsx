a59f7b4e28600d9b578f7b17d5cdf6bdde97eb1e
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-01 15:08:09 -0500

    App-configure NavigationContainer

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import {  Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <SafeAreaView className='flex-1 items-center justify-center bg-red-500'>
-      <Text className='text-white'>This should be working!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView className='flex-1 items-center justify-center bg-red-500'>
+        <Text className='text-white'>This should be working!</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
