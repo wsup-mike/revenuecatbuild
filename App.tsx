769c84a8d4a872db4ec32aaea6cb5f1e029d7546
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-01 15:27:40 -0500

    App-Remove Home from top

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
