75afbd9081325b88169434cd71d21a29e54385c4
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-01 15:19:00 -0500

    App-Configure new StackNavigator n cr8 new screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import {  Text, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+
   return (
     <NavigationContainer>
-      <SafeAreaView className='flex-1 items-center justify-center bg-red-500'>
-        <Text className='text-white'>This should be working!</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
