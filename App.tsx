5d975a00cd5f07430d7331deee12aaea33171135
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-01 14:58:53 -0500

    App.tsx-apply stying now that className ready

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import {  Text, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView className='p-10'>
-      <Text>This should be working!</Text>
+    <SafeAreaView className='flex-1 items-center justify-center bg-red-500'>
+      <Text className='text-white'>This should be working!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
