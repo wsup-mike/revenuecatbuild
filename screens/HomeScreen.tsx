bfda8badb43de1223137ac1ca81034c9e7f07714
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-01 15:24:09 -0500

    App-import HomeScreen component

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,9 @@
 import { View, Text } from 'react-native'
-import React from 'react'
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text>This is the HomeScreen</Text>
     </View>
   )
 }
