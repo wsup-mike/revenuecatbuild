68367c0bc72b584b0e16f8bff7221c65ae02fd62
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-02 08:04:47 -0500

    Add the NavigationProp

--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -1,5 +1,12 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../App';
+
+export type NavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Demo"
+>;
 
 const DemoScreen = () => {
   return (
