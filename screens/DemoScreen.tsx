4e2c9779eedb938806c5ed76d03b5d52f38cfda3
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-02 08:07:15 -0500

    import useNavigation

--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -2,6 +2,7 @@ import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
+import { useNavigation } from '@react-navigation/native';
 
 export type NavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -9,6 +10,7 @@ export type NavigationProp = NativeStackNavigationProp<
 >;
 
 const DemoScreen = () => {
+  
   return (
     <SafeAreaView>
       <Text>DemoScreen</Text>
