30384c45a6fe4b8efe3398d355f9ff117c81e886
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-02 15:22:33 -0500

    Cr8 the navigation object

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -11,70 +11,73 @@ export type NavigationProp = NativeStackNavigationProp<
 >;
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView className='flex-1 bg-gray-100 relative'>
-        <ScrollView>
-            {/* Pro/UpgradeIconButton */}
-            <TouchableOpacity className='absolute z-50 top-5 right-10 items-center' onPress={navigation.navigate}>
-                <Ionicons name={'person-circle'} size={24} color='#E5962D'/>
-                <Text className='text-center text-[#E5962D]'>PRO</Text>
-            </TouchableOpacity>
 
-            {/* Image */}
-            <Image 
-                source={{ uri:'https://i.imgur.com/e14NE49.png'}} 
-                className='w-full h-48'
-            />
+    const navigation = useNavigation<NavigationProp>();
 
-            {/* Top ActionRow */}
-            <View className='mx-5 '>
-                <View className='flex-row justify-between space-x-2'>
+    return (
+        <SafeAreaView className='flex-1 bg-gray-100 relative'>
+            <ScrollView>
+                {/* Pro/UpgradeIconButton */}
+                <TouchableOpacity className='absolute z-50 top-5 right-10 items-center' onPress={navigation.navigate}>
+                    <Ionicons name={'person-circle'} size={24} color='#E5962D'/>
+                    <Text className='text-center text-[#E5962D]'>PRO</Text>
+                </TouchableOpacity>
+
+                {/* Image */}
+                <Image 
+                    source={{ uri:'https://i.imgur.com/e14NE49.png'}} 
+                    className='w-full h-48'
+                />
+
+                {/* Top ActionRow */}
+                <View className='mx-5 '>
+                    <View className='flex-row justify-between space-x-2'>
+                        <ActionRow 
+                            title='Track Workout'
+                            screen='Demo'
+                            color='#E5962D'
+                            icon='fitness'
+                            vertical
+                        />
+                        <ActionRow 
+                            title='Browse Workouts'
+                            screen='Demo'
+                            color='#1982C4'
+                            icon='library'
+                            vertical
+                        />
+                    </View>
                     <ActionRow 
-                        title='Track Workout'
+                        title='Connect with Friends'
                         screen='Demo'
-                        color='#E5962D'
-                        icon='fitness'
-                        vertical
+                        color='#F44174'
+                        icon='share-social'
                     />
                     <ActionRow 
-                        title='Browse Workouts'
+                        title='Add an Exercise'
                         screen='Demo'
-                        color='#1982C4'
-                        icon='library'
-                        vertical
+                        color='#8AC926'
+                        icon='add-circle'
+                        requiresPro
+                    />
+                    <ActionRow 
+                        title='Create a Routine'
+                        screen='Demo'
+                        color='#C03221'
+                        icon='md-time'
+                        requiresPro
+                    />
+                    <ActionRow 
+                        title='Join Challenge'
+                        screen='Demo'
+                        color='#23967F'
+                        icon='trophy'
+                        requiresPro
                     />
                 </View>
-                <ActionRow 
-                    title='Connect with Friends'
-                    screen='Demo'
-                    color='#F44174'
-                    icon='share-social'
-                />
-                <ActionRow 
-                    title='Add an Exercise'
-                    screen='Demo'
-                    color='#8AC926'
-                    icon='add-circle'
-                    requiresPro
-                />
-                <ActionRow 
-                    title='Create a Routine'
-                    screen='Demo'
-                    color='#C03221'
-                    icon='md-time'
-                    requiresPro
-                />
-                <ActionRow 
-                    title='Join Challenge'
-                    screen='Demo'
-                    color='#23967F'
-                    icon='trophy'
-                    requiresPro
-                />
-            </View>
-      </ScrollView>
-    </SafeAreaView>
-  )
+        </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 export default HomeScreen
