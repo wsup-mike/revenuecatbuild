9f4907c08badcfad3e1a8d38a0c40722e1445deb
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-02 15:13:52 -0500

    Cr8 new screen: PaywallScreen.tsx

---
+++ screens/PaywallScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const PaywallScreen = () => {
+  return (
+    <View>
+      <Text>PaywallScreen</Text>
+    </View>
+  )
+}
+
+export default PaywallScreen
