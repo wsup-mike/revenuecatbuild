9a6cf6ce52003677c7fe02dfbb21d13c10c12378
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-02 14:31:06 -0500

    Add text and the wrench icon and other text too

--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -18,7 +18,18 @@ const DemoScreen = () => {
         <Ionicons name='arrow-back' size={50} color='white' />
         <Text className='text-white'>Go back</Text>
       </TouchableOpacity>
-      <Text>DemoScreen</Text>
+      
+      <View className='flex-1 justify-center items-center px-10'>
+        <Text className='text-white text-2xl font-extrabold'>Hooray!</Text>
+        <Text className='text-white text-2xl font-extrabold mb-20 text-center'>You have access to this feature!</Text>
+        <Ionicons className='' name='build-outline' size={200} color='white' />
+        <View className='-mt-16 -ml-8'>
+          <Ionicons name='checkmark-circle-sharp' size={60} color='#96f550' />
+        </View>
+        <Text className='flex-1 text-white font-bold text-center mt-10'>This is where you createa a new screen and change the rest of the app so the navigation is as follows: navigation.navigate('ThePageYouWant')</Text>
+
+      </View>
+
     </SafeAreaView>
   )
 }
