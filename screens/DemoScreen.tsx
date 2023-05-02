8cbb60cfdffb0835ec061dcb6620fdfb6ad2540e
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-02 13:58:12 -0500

    Import n Insert Ionicons

--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -1,5 +1,6 @@
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
 import { useNavigation } from '@react-navigation/native';
