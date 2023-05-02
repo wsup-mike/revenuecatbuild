575453b5b1495a860437764ef887fa35888f4426
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-02 16:18:55 -0500

    Cr8 the 'navogation'object

--- screens/PaywallScreen.tsx
+++ screens/PaywallScreen.tsx
@@ -10,32 +10,34 @@ export type NavigationProp = NativeStackNavigationProp<
 >;
 
 const PaywallScreen = () => {
-  return (
-    <SafeAreaView className='flex-1 bg-[#1a2f44] items-center p-6'>
-        <TouchableOpacity>
-            <Ionicons name='md-close-circle-sharp' size={32} color={'#e5962d'}/>
-        </TouchableOpacity>
-        <Text className='text-white text-2xl font-extrabold'>UPGRADE</Text>
-        <Text className='text-white mt-2'>Upgrade to Pro to Access all the Features</Text>
-        <Ionicons />
-        <View>
-            {/* Access to all pro features */}
-            <View>
-                <Ionicons />
-                <View></View>
-            </View>
+    const navigation = useNavigation<NavigationProp>();
 
-            {/* Access to all pro features */}
-
-            {/* Access to all pro features */}
+    return (
+        <SafeAreaView className='flex-1 bg-[#1a2f44] items-center p-6'>
             <TouchableOpacity>
-                <Text></Text>
-                <Text></Text>
+                <Ionicons name='md-close-circle-sharp' size={32} color={'#e5962d'}/>
             </TouchableOpacity>
-            <Text>Restore Purchases</Text>
-        </View>
-    </SafeAreaView>
-  )
+            <Text className='text-white text-2xl font-extrabold'>UPGRADE</Text>
+            <Text className='text-white mt-2'>Upgrade to Pro to Access all the Features</Text>
+            <Ionicons />
+            <View>
+                {/* Access to all pro features */}
+                <View>
+                    <Ionicons />
+                    <View></View>
+                </View>
+
+                {/* Access to all pro features */}
+
+                {/* Access to all pro features */}
+                <TouchableOpacity>
+                    <Text></Text>
+                    <Text></Text>
+                </TouchableOpacity>
+                <Text>Restore Purchases</Text>
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default PaywallScreen
