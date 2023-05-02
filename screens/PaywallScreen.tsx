2e5b9280f5efd1b8f2bdb8f1d16fa7480a04b3d6
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-02 16:13:59 -0500

    import NativeStackHeaderProps

--- screens/PaywallScreen.tsx
+++ screens/PaywallScreen.tsx
@@ -1,5 +1,6 @@
 import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
+import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 
 const PaywallScreen = () => {
   return (
