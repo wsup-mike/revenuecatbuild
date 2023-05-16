1a1af72cf5d61ffe2c4f4c47af70d55297785cbc
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-16 11:33:41 -0500

    HomeScrn-import useRevenueCat

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,6 +4,7 @@ import ActionRow from '../components/ActionRow';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
 import { useNavigation } from '@react-navigation/native';
+import useRevenueCat from '../hooks/useRevenueCat';
 
 export type NavigationProp = NativeStackNavigationProp<
     RootStackParamList,
@@ -13,6 +14,7 @@ export type NavigationProp = NativeStackNavigationProp<
 const HomeScreen = () => {
 
     const navigation = useNavigation<NavigationProp>();
+    const { currentOffering, customerInfo, isProMember } = useRevenueCat();
 
     return (
         <SafeAreaView className='flex-1 bg-gray-100 relative'>
