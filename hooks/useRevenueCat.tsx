35e52c03a72da848a086418fc6afa228fa3cb913
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-16 10:07:22 -0500

    useRevenueCat-imports and apiKeys

---
+++ hooks/useRevenueCat.tsx
@@ -0,0 +1,16 @@
+// Creating a custom hook
+import { Platform } from "react-native"; // to detect the type of OS user is on
+import Purchases from "react-native-purchases/dist/purchases"; // to import from react native purchases
+import { CustomerInfo } from "react-native-purchases/dist/customerInfo";
+import { PurchasesOffering } from "react-native-purchases/dist/offerings";
+
+const apiKeys = { // Normally we should put this in a .env file!
+    apple: 'appl_WlRlUBLoWljLTsqoPspfNfEYkwj', // The RevenueCat API key for iOS build
+    google: '', // The RevenueCat API key for Androids
+}
+
+function useRevenueCat() {
+
+}
+
+export default useRevenueCat;
